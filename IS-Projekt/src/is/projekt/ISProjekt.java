531686c9522cc2aa90a5c058eee14e256ec87a18
Eric Nordlund <eric@epitet.net>
2012-12-05 14:30:42 +0100

    Andra....

--- IS-Projekt/src/is/projekt/ISProjekt.java
+++ IS-Projekt/src/is/projekt/ISProjekt.java
@@ -7,9 +7,7 @@ package is.projekt;
 
 public class ISProjekt {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
     } 
