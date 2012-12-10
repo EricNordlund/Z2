b91de73bdaa0e1211f577df8f57f4ebcceacbfff
Eric Nordlund <eric@epitet.net>
2012-12-10 13:26:24 +0100

    Fixade det sista med orderrad <-> order.

---
+++ IS-Projekt/src/is/controller/Controller.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package is.controller;
+
+/**
+ * Detta är en klass som kör gui samt applikation och samordnar dessa.
+ * 
+ * 
+ * 
+ * 
+ */
+public class Controller {
+    
+}
