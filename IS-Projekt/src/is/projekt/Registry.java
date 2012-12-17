7d64cd89afbe4ee0bfe8916a7917fa44b316dff2
Eric Nordlund <eric@epitet.net>
2012-12-17 11:50:36 +0100

    Bytte namn på Model -> Registry

--- IS-Projekt/src/is/projekt/Registry.java
+++ IS-Projekt/src/is/projekt/Registry.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package is.projekt;
 
 import java.io.Serializable;
@@ -11,7 +7,7 @@ import java.util.HashMap;
  *
  * @author svalan
  */
-public class Model implements Serializable {
+public class Registry implements Serializable {
 
     private HashMap<Integer, Boat> boatRegistry = new HashMap<>();
     private HashMap<Integer, Customer> customerRegistry = new HashMap<>();
